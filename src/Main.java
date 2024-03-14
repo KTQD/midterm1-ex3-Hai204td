2a201b1b1a3b23f8453c57f0410c742eff6799a6
Hai204td <157661199+Hai204td@users.noreply.github.com>
2024-03-14 08:35:33 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String inputFileName = "input.txt";
+        String outputFileName = "output.txt";
+        replaceText(inputFileName, outputFileName, "Nha Trang", "Vũng Tàu");
+    }
 
+    public static void replaceText(String inputFileName, String outputFileName, String oldText, String newText) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
 
-        Bài làm viết tại đây
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll(oldText, newText);
+                writer.write(line);
+                writer.newLine();
+            }
+            System.out.println("Thay the thanh cong");
 
+        } catch (IOException e) {
+            System.err.println("Bao loi :" + e.getMessage());
+        }
     }
 }
